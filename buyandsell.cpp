c73a93f561b404a6a5cc2edc7acb50d84ecb227b
Emanijaz15 <eijaz7357@gmail.com>
2026-03-11 14:10:06 +0500

    Update buyandsell.cpp

--- buyandsell.cpp
+++ buyandsell.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int minPrice=INT_MAX;
-        int maxProfit =0;
+        int maxProfit =0; 
         for(int i=0;i<prices.size(); i++){
             if(prices[i] < minPrice){
                 minPrice = prices[i];
@@ -13,4 +13,5 @@ public:
         }
         return maxProfit;
     }
+
 };
